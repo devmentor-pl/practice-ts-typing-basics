64e6eec87f04b00b377cbe7ea44bc81768554aac
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2025-01-20 08:42:19 +0100

    end task 01

---
+++ 01/app.js
@@ -0,0 +1,6 @@
+var setTheme = function (theme) {
+    return "Ustawiono motyw ".concat(theme);
+};
+console.log(setTheme('light'));
+console.log(setTheme('dark'));
+console.log(setTheme('system'));
